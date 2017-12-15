8426acd36abd1756083536ceae0cd24115469f7c
Diana <dianastanciu13@yahoo.co.uk>
2017-12-15 14:11:11 +0200

    carousel code styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
+//DIANA - SERVICES START
+
 $(document).ready(function() {
-    //DIANA - SERVICES START
     $(".image").on("mouseenter", function() {
         $(".button").addClass("showBtn");
     });
-    //DIANA - SERVICES END
-
-
-
-      
+     
 });
 
 
 //CAROUSEL TEST 
 $(function() {
     new Dragdealer('image-carousel', {
-        steps: 4,
-        speed: 0.3,
-        loose: true
+      steps: 4,
+      speed: 0.3,
+      loose: true
     });
-})
+  })
+  
+
+//DIANA - SERVICES END   
