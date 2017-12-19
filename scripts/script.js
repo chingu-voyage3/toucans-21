ded49ec6079735eefd156e3f606ead1d8d5c6c21
Diana <dianastanciu13@yahoo.co.uk>
2017-12-19 14:21:52 +0200

    add nav styles

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,17 @@ $(document).ready(function() {
         $(".button").addClass("showBtn");
     });
      
+
+    var allowAnimations = function () {
+        $(".items").removeClass("disableAnimation");
+    };
+
+
+    $(".burger").on("click", function() {
+        allowAnimations();
+        $(".burger-menu").toggleClass("hide");
+        $(".burger-x").toggleClass("show");
+        $(".items").toggleClass("animateItems");
+    });
 });
 //DIANA - SERVICES END   
